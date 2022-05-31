25f4145804de41ac02fc4e0bb1d568ea62e61f40
Vlad Zubin <zoobeen1@gmail.com>
2022-05-31 14:45:33 +0300

    Update axis-crud.js

--- src/js/axis-crud.js
+++ src/js/axis-crud.js
@@ -2,7 +2,7 @@ const PIXA_KEY = '27472864-20a91975f294fe19c608dc0e7';
 const BASE_URL = 'https://pixabay.com/api';
 const perPage = 40;
 const PARAMS = `per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
-import Axios from 'Axios';
+import Axios from 'axios';
 //functions
 //create
 //read
