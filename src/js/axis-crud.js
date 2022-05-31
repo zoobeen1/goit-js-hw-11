e692b6517b3efeb718948b6b1c2ee84472f00926
Vlad Zubin <zoobeen1@gmail.com>
2022-05-31 21:46:41 +0300

    Update axis-crud.js

--- src/js/axis-crud.js
+++ src/js/axis-crud.js
@@ -2,6 +2,9 @@ const PIXA_KEY = '27472864-20a91975f294fe19c608dc0e7';
 const BASE_URL = 'https://pixabay.com/api';
 
 import Axios from 'axios';
+const instance = Axios.create({
+  baseURL: BASE_URL,
+});
 //functions
 //read
 async function getPhotos(searchQuery, page, perPage) {
@@ -14,7 +17,7 @@ async function getPhotos(searchQuery, page, perPage) {
     per_page: perPage,
     page: page,
   };
-  const resp = await Axios.get(`${BASE_URL}`, { params });
+  const resp = await instance.get('', { params });
   return await resp.data;
 }
 export default { getPhotos };
