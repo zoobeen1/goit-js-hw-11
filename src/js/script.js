f75491d0b101b37f5793e2758f538cf4f9f787e8
Vlad Zubin <zoobeen1@gmail.com>
2022-05-31 20:57:15 +0300

    some-fixes

--- src/js/script.js
+++ src/js/script.js
@@ -21,6 +21,13 @@ function renderGallery(data) {
   const photoCard = photoCardTpl(data);
   gallery.insertAdjacentHTML('beforeend', photoCard);
   lightbox.refresh();
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
 function clearGallery() {
   gallery.innerHTML = '';
